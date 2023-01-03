55233de75eca7666cf8f1c565550fa3fd5425b90
Michael Cohen <michael.cohen@globality.com>
2023-01-02 21:49:47 -0800

    Updated host app project

---
+++ host-app/src/d/action1.js
@@ -0,0 +1,7 @@
+function doHostAction1() {
+    console.log("host action1 invoked");
+}
+
+module.exports = {
+    doHostAction1,
+};
---
+++ host-app/src/d/action2.js
@@ -0,0 +1,7 @@
+function doHostAction2() {
+    console.log("host action2 invoked");
+}
+
+module.exports = {
+    doHostAction2,
+};
